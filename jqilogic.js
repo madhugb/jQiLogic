dcd7c79cf4b53869e64c545a17f3a002e53904cb
Madhu Halemane <madhuhalemane@ubuntu.ubuntu-domain>
2012-08-13 14:06:22 +0530

    jqilogic core file

---
+++ jqilogic.js
@@ -0,0 +1,25 @@
+/*
+ * jQiLogic, jQuery in-line logic operation
+ * Copyright(c) 2012 Madhu <madhuvana@gmail.com>
+ * https://github.com/madhugb/jQiLogic
+ * 
+ */
+ 
+(function($){  
+  $.fn.extend({    
+    //
+    // Logic If 
+    // @args[0] = condition;
+    // @args[1] = value to append if condition is true;
+    // @args[2] = value to append if condition is false;
+    //    
+    appendIf: function() {      
+      if (arguments[0]) {        
+        $(this).append(arguments[1]);
+      } else {     
+        $(this).append(arguments[2]);     
+      }
+      return this;
+    }    
+  });
+})(jQuery);
